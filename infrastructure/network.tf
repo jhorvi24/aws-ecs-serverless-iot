46f0ec4d8bc1cf8eaf93913892df5c0bf3c5cbe3
Jhorman Villanueva <jhorvi24@hotmail.com>
2026-04-06 20:57:18 -0500

    Terraform ready

--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -216,10 +216,10 @@ resource "aws_security_group" "grafana-sg" {
   vpc_id      = aws_vpc.iot-ecs-red.id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb-iot-sg.id]
   }
 
 
